8ca5c94c68f3b3ac08876f6226f71908de0c9e93
Greg Nuccio <greg.nuccio@jeppesen.com>
2025-08-18 12:13:07 -0600

    chore: Update version

--- token-service/src/fetch/data-icons.ts
+++ token-service/src/fetch/data-icons.ts
@@ -7,7 +7,6 @@ import 'dotenv/config';
 export async function saveFigmaFileData() {
   // Fetch data from Figma
   console.log('Fetching icons from FIGMA API...');
-  console.log('Fetching file: ', process.env.FIGMA_ICONS_FILE_KEY);
   const iconsData: FigmaImportResult = await importFromFigma({
     file: process.env.FIGMA_ICONS_FILE_KEY || '',
     token: process.env.FIGMA_API_KEY || '',
@@ -15,14 +14,14 @@ export async function saveFigmaFileData() {
     pages: ['Atmosphere Icons'],
     iconNameForNode: (node) => {
       if (
-        node.parents.length !== 3
+        node.parents.length !== 3 
       ) {
           return null;
       }
       return node.name;
     }
   });
-
+  
   mkdirSync(Const.dataPaths.dir, { recursive: true });
   writeFileSync(Const.dataPaths.data.icons, JSON.stringify(iconsData, null, 2));
 }
